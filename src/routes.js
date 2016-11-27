e1df8699f4d1fff8e4b82f9e17fdd31b52c960e8
John Coxhead <jcoxhead@hotmail.com>
2016-11-27 14:55:20 +0000

    Added population of authors

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,13 @@ import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/courses/CoursesPage';
 // import CoursesPage from './components/course/CoursesPage';
-// import ManageCoursePage from './components/course/ManageCoursePage'; //eslint-disable-line import/no-named-as-default
+import ManageCoursePage from './components/courses/ManageCoursePage'; 
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="/coursesPage" component={CoursesPage} />
+    <Route path="/course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
